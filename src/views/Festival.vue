7b7091ef04527faf62b276498d7d1faff5800b9d
Pasquarelli-Theo <theo.pasquarelli@edu.univ-fcomte.fr>
2022-05-31 19:22:36 +0200

    responsive4

--- src/views/Festival.vue
+++ src/views/Festival.vue
@@ -22,11 +22,11 @@
       </div>
       <img
         src="../../public/img/damso_festival.jpg"
-        class="p-5 mt-5 lg:h-60 lg:w-screen lg:object-cover md:h-64 md:w-screen md:object-cover"
+        class="mt-5 p-5 md:h-64 md:w-screen md:object-cover lg:h-60 lg:w-screen lg:object-cover"
       />
-      <div class="mt-9 ml-5 mr-5 bg-white p-2">
-        <p class="p-2 font-bold">Plus d’info</p>
-        <p>
+      <div class="mt-9 ml-5 mr-5 p-2">
+        <p class=" bg-white p-2 font-bold">Plus d’info</p>
+        <p class="bg-white">
           Le festival 13 BPM se déroule à la Plage du Prado dans le 8ème
           arrondissement de Marseille. Le 13 BPM aura lieu le 11 et 12 Juin
           2022, les premiers concerts commenceront dès 19h et cela jusqu’à
@@ -39,7 +39,7 @@
       </div>
       <img
         src="../../public/img/hanny-naibaho-aWXVxy8BSzc-unsplash.jpg"
-        class="p-5"
+        class="p-5 mt-5 md:h-64 md:w-screen md:object-cover lg:h-60 lg:w-screen lg:object-cover"
       />
     </div>
   </div>
